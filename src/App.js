db92d7e42a6c42c6ef51aa3e1010bb7fb510600b
matheus de souza <matheussouza18@live.com>
2019-09-12 13:58:58 -0300

    Basic routes created

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import Rooms  from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route , Switch } from 'react-router-dom';
 
 function App() {
-  return <>
-  <Home></Home>
-  <Rooms></Rooms>
-  <SingleRoom></SingleRoom>
-  <Error></Error>
-  </>;
+  return (
+  < >
+  <Switch>
+  <Route exact path="/" component = {Home}/>
+  <Route exact path="/rooms/" component = {Rooms}/>
+  <Route exact path="/rooms/:slug" component = {SingleRoom}/>
+  <Route component = {Error} />
+  </Switch>
+   </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+  <Router>
+  <App />
+  </Router>,
+  document.getElementById("root"));
+
+
+
+serviceWorker.unregister();

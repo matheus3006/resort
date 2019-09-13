353753e7f604d5af5a730467b3ff343609976ee3
matheus de souza <matheussouza18@live.com>
2019-09-13 07:20:12 -0300

    ultimo commit antes do togle, modificação no css do arquivo para deixar o hamburger visivvel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import logo from '../images/logo.svg';
 import {FaAlingnRight} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 
 export default class NavBar extends Component {
+  state={
+    isOpen:false;
+  }
   render() {
     return (
       <div>

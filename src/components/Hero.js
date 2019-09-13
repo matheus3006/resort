321d5c3915e5e21d702d6660425bc0e02a62e361
matheus de souza <matheussouza18@live.com>
2019-09-13 08:03:57 -0300

    add hero component, mas por alguma razão a imagem de background n aparece

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Hero( {children, hero } ) {
+  return <header className= {hero} > {children} </header>;
+}
+
+Hero.defautProps = {
+  hero: "defautHero"
+};

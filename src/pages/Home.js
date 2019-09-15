7629a1ab43d51b474db83030c1fc13153779e94b
matheus de souza <matheussouza18@live.com>
2019-09-15 10:21:45 -0300

    Adicionado Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
-import Hero from '../components/Hero';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 // import { Container } from './styles';
 
 const Home = () => {
-  return <Hero hero = "defaultHero"/>
-}
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="Btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
 
 export default Home;

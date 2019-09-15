9db348e220ce774660ecdd7743c8dcb8cde66654
matheus de souza <matheussouza18@live.com>
2019-09-15 11:55:06 -0300

    Add services , and ended components fase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import Services from '../components/Services'
 // import { Container } from './styles';
 
 const Home = () => {
   return (
+    <>
     <Hero>
       <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
         <Link to="/rooms" className="btn-primary">
@@ -13,6 +15,8 @@ const Home = () => {
         </Link>
       </Banner>
     </Hero>
+    <Services/>
+    </>
   );
 };
 

b1c5b6416663c3da80c90587692ecbdc392e1c63
matheus de souza <matheussouza18@live.com>
2019-09-16 10:59:01 -0300

    tempItems add

--- src/context.js
+++ src/context.js
@@ -12,16 +12,19 @@ class RoomProvider extends Component {
   };
   //getData { }
   componentDidMount(){
+    //this.getData
     let rooms= this.formatData(items)
+    console.log(rooms);
   }
   formatData(items){
     let tempItems = items.map(item =>{
       let id = item.sys.id;
-      let images = items.fields.images.map(image =>  image.fields.file.url);
+      let images = item.fields.images.map(image =>  image.fields.file.url);
 
-
-
+      let room = {...item.fields ,images:images,id}
+      return room;
     });
+    return tempItems;
   }
 
   render() {

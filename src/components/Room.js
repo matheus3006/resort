4752ab24cb860b9c3501019189b804baa23a9ac5
matheus de souza <matheussouza18@live.com>
2019-09-17 10:17:18 -0300

    Created new features, and link to single rooms page

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 // import { Container } from './styles';
+import defaultImg from '../images/room-1.jpeg';
+export default function Room( {room} ) {
+  const {name, slug , images, price} = room;
 
-export default function Room() {
   return (
-    <div>Hello from Room</div>
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="Single Room"/>
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>  
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">Features</Link> 
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
   );
 }

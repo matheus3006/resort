0b3db7f6646ca62b218379cbdf30b25a91d79968
matheus de souza <matheussouza18@live.com>
2019-09-21 16:07:18 -0300

    New way add

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,27 +1,47 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomList from "./RoomList";
-import { RoomConsumer } from "../context";
+import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
-// import { Container } from './styles';
 
-export default function RoomContainer() {
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {
-      (value) => {
-        const {loading,sortedRooms,rooms} = value
-        if(loading){
-          return <Loading/>;
-        }
-        return (
-          <div>
-            Hello from rooms Container
-            <RoomsFilter rooms={rooms} />
-            <RoomList rooms={sortedRooms} />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <>
+      <RoomList rooms={sortedRooms} />
+      <RoomsFilter rooms={rooms} />
+    </>
   );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomsFilter from "./RoomsFilter";
+// import RoomList from "./RoomList";
+// import { RoomConsumer } from "../context";
+// import Loading from "./Loading";
+// // import { Container } from './styles';
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from rooms Container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }

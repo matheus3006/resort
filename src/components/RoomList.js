373d7d93b4bfb62b37afe2aebde9ba94bf0b6547
matheus de souza <matheussouza18@live.com>
2019-09-21 16:14:55 -0300

    add the rooms to the Room component

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Room from './Room';
 
 // import { Container } from './styles';
 
-export default function RoomList() {
+export default function RoomList( {rooms} ) {
+  if(rooms.lenght === 0){
+    return(
+      <div className="empty-search">
+        <h3>Unfortunately there are no rooms matched your search parameters</h3>
+      </div>
+
+    )
+    
+  }
   return (
-    <div>hello from room list </div>
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {
+          rooms.map(item=>{
+            return <Room key={item.id} room={item}/>;
+          })
+        }
+      </div>
+    </section>
   );
 }

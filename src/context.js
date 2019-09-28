9951ed1453d17a82499bec10753309e36325e59e
matheus de souza <matheussouza18@live.com>
2019-09-28 13:28:53 -0300

    Add the check by type of room

--- src/context.js
+++ src/context.js
@@ -53,13 +53,26 @@ class RoomProvider extends Component {
     return room;
   };
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target
+    const value = event.type === 'checkbox'? target.checked : target.value
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type, name, value);
+    this.setState({
+      [name] : value
+    }, this.filterRooms);
+    
   };
   filterRooms = () => {
-    console.log("filter");
+    let {
+      rooms, type, capacity ,  price, minSize, maxSize, breakfast , pets
+    }=this.state;
+    let tempRooms = [... rooms];
+    if(type !== 'all'){
+      tempRooms = tempRooms.filter( (room => room.type === type))
+    }
+    this. setState({
+      sortedRooms : tempRooms
+    })
+
   };
   render() {
     return (

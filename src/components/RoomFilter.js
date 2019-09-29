65cdcf5f6b1185d1cd3d7e8a367c36dd26b4cdfc
matheus de souza <matheussouza18@live.com>
2019-09-29 10:49:43 -0300

    Add room price filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,51 +1,97 @@
-import React from 'react';
-import {useContext} from 'react';
-import{RoomContext} from '../context';
-import Title from '../components/Title';
+import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
 //Get all unique values
-const getUnique = (items,value)=>{
- return [... new Set(items.map(item=> item[value]))]
-}
-export default function RoomFilter( {rooms}) {
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+export default function RoomFilter({ rooms }) {
   const context = useContext(RoomContext);
-  const {handleChange,type,capacity,price,minPrice,MaxPrice,minSize,maxSize,breakfast,pets}=context;
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    MaxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
   //get unique types
-  let types = getUnique(rooms, 'type');
- //add All
- types = ['all' , ...types];
- // map to jsx
- types = types.map((item,index)=>{
-   return <option value={item} key = {index}>{item}</option>
- })
-let people = getUnique(rooms, 'capacity');
-people = people.map((item,index)=>{
-  return <option key= {index} value= {item}>{item}</option>
-})
+  let types = getUnique(rooms, "type");
+  //add All
+  types = ["all", ...types];
+  // map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  let people = getUnique(rooms, "capacity");
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
 
   return (
-   <section className="filter-container">
-     <Title title="search Rooms"/>
-     <form action="" className="filter-form">
-       {/* select type */}
-      <div className="form-group">
-        <label htmlFor="type">room-type</label>
-        <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
-          {types}
-        </select>
-      </div>
+    <section className="filter-container">
+      <Title title="search Rooms" />
+      <form action="" className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room-type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
 
-       {/* end select type */}
-       {/* select guests */}
-      <div className="form-group">
-        <label htmlFor="capacity">Guest</label>
-        <select name="capacity" id="capacity" value={capacity} className="form-control" onChange={handleChange}>
-          {people}
-        </select>
-      </div>
+        {/* end select type */}
+        {/* select guests */}
+        <div className="form-group">
+          <label htmlFor="capacity">Guest</label>
+          <select
+            name="capacity"
+            id="capacity"
+            value={capacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+        {/* end select guests */}
 
-       {/* end select guests */}
-     </form>
-      </section>
+        {/* Room price */}
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        {/* end of room price  */}
+      </form>
+    </section>
   );
 }
